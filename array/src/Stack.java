3bcb2aff183bfdd124b5ba1c8d15e96d7aa50f2e
doanvt190 <158265477+doanvt190@users.noreply.github.com>
2024-12-10 12:39:59 +0700

    Add files via upload

---
+++ array/src/Stack.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+import java.util.EmptyStackException;
+class StudentStack {
+    private Stack<Student> stack = new Stack<>();
+    public void push(Student student) {
+        stack.push(student);
+    }
+    public Student pop() {
+        if (!stack.isEmpty()) {
+            return stack.pop();
+        } else {
+            throw new EmptyStackException();
+        }
+    }
+    public Student peek() {
+        if (!stack.isEmpty()) {
+            return stack.peek();
+        } else {
+            throw new EmptyStackException();
+        }
+    }
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+    public void displayStack() {
+        if (stack.isEmpty()) {
+            System.out.println("Stack is empty.");
+        } else {
+            for (Student student : stack) {
+                System.out.println(student);
+            }
+        }
+    }
+}
+
+
